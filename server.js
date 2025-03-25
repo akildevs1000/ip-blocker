547cafaac5e7ab2efcb73d1ce7630d1ab43ea934
akildevs1000 <akildevs1000@gmail.com>
2025-03-25 14:38:24 +0400

    test

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ fs.readFile(logFile, 'utf8', (err, data) => {
         console.error('Error reading log file:', err);
         return;
     }
-    
+
     // Match IPs from "Invalid user" and "Failed password for invalid user" logs
     const ipRegex = /Invalid user .* from ([\d.]+) port|Failed password for invalid user .* from ([\d.]+) port/g;
     let match;
     const attackerIps = new Set();
-    
+
     while ((match = ipRegex.exec(data)) !== null) {
         const ip = match[1] || match[2];
         if (ip && !whitelistedIps.includes(ip)) {
             attackerIps.add(ip);
         }
     }
-    
-    console.log('Attacker IPs:', [...attackerIps]);
+
+    console.table([...attackerIps]);
 });
